475f4831633d1e274e80a7c21f60ee793a86d207
chandan solanki <chandansolanki618@gmail.com>
2024-06-02 12:46:08 +0530

    creator section add

--- js/desktop.js
+++ js/desktop.js
@@ -1,3 +1,19 @@
+// ----------- SET COPY RIGHT DATE SECTION ------------
+
+const copyright_date = document.querySelector(".copyright-date");
+
+
+window.addEventListener("load", () => {
+    setCopyRightDate();
+})
+
+function setCopyRightDate() {
+    let date = new Date();
+    date = date.getFullYear();
+    copyright_date.innerHTML = `<p class="copyright-date">Copyright © nseditingservice <span class="footer-logo"><img src="/images/NS Editing Service Logo.png" alt="ns_log"></span> ${date}</p>`
+}
+
+
 
 // <!-- SCRIPT FOR SIDEMENU BAR FOR RESPONSIVE DESIGN -->
 
@@ -34,7 +50,8 @@ function opentab(tabname) {
 
 
 // <!-- SCRIPT FOR CONTACT FORM SUBMISSION -->
-const scriptURL = 'https://script.google.com/macros/s/AKfycbySEcUXIXGi_IIktTXoU-oLgtLgZySoQfTJdbmfRTwoNni4e_iv_nXkxCoyYBqN-nFJ/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxmU5fpv07B2PbKhK3AysBphzU9YmxHCf6d8H8DXFJuwdXC5GvK0T-s5ESyBynPQaYA/exec';
+
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById('msg');
 const tick = document.getElementById('tick');
@@ -54,3 +71,100 @@ form.addEventListener('submit', e => {
         })
         .catch(error => console.error('Error!', error.message))
 })
+
+
+
+
+
+
+
+
+
+
+
+// --------------------- GENERATE CREATORS --------------
+
+
+const main_creator_conatanier = document.querySelector(".main-creator-conatanier");
+
+
+let channelIdArr = ["UC9dyrsYEmD4iGJ8Oz1G5dpw", "UCiGyWN6DEbnj2alu7iapuKQ" ,"UC3N9i_KvKZYP4F84FPIzgPQ", "UC-CSyyi47VX1lD9zyeABW3w" , "UCneyi-aYq4VIBYIAQgWmk_w" , "UCj22tfcQrWG7EMEKS0qLeEg" , "UCsDTy8jvHcwMvSZf_JGi-FA", "UCzwCEE_PchiBULMnAJqhGVg"];
+
+
+async function fetchData() {
+    
+    const promises = channelIdArr.map((id) => getData(id));
+
+    console.log(promises)
+
+    try {
+
+        const res = await Promise.allSettled(promises);
+        // console.log(res);
+
+        res.forEach((provalue) => {
+            if (provalue.status === "fulfilled") {
+                createCreator(provalue.value);
+            } else {
+                console.log("error ", provalue.reason)
+            }
+        });
+
+        setLastEl();
+    }
+    catch (err) {
+        console.log(err.message)
+    }
+}
+
+
+async function getData(channelId) {
+    const url = `https://yt-api.p.rapidapi.com/channel/about?id=${channelId}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ec6c980ec1msh2cf222d5db03180p1d0350jsnc6b46f4da18f',
+            'X-RapidAPI-Host': 'yt-api.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        // console.log(result);
+        // createCreator(result);
+        return result;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+
+function createCreator(data) {
+    let elcreatordiv = generateCreator(data);
+    main_creator_conatanier.innerHTML += elcreatordiv;
+}
+
+function generateCreator(data) {
+    let channelname = data.title;
+    let subscriberCnt = data.subscriberCountText;
+    let logoUrl = data.avatar[2].url;
+
+    return `<div class="cards">
+    <div class="creator-img">
+        <img src="${logoUrl}" alt="creator img">
+    </div>
+    <p class="creator-name">${channelname}</p>
+    <p class="creator-subscriber">${subscriberCnt} Subscriber </p>
+    </div>`;
+
+}
+
+function setLastEl() {
+    let lastEL = `<div class="manyMore">
+                <p>and many more......</p>
+                </div>`;
+    main_creator_conatanier.innerHTML += lastEL;
+}
+
+fetchData()
